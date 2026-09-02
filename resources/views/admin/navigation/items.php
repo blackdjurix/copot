d5704b2cdb5082eaaea5590502e87a93783b65c2
blackdjurix <blackdjurix@gmail.com>
2026-09-02 14:45:49 +0700

    fix(wu3): refresh navigation dismissal asset

--- resources/views/admin/navigation/items.php
+++ resources/views/admin/navigation/items.php
@@ -39,4 +39,4 @@ $renderTree = function (array $nodes) use (&$renderTree, $esc, $selectedId, $nav
         </div>
     </section>
 </div>
-<script defer src="<?= $esc('/admin-assets/js/navigation-admin.js?v=wu3-acceptance') ?>"></script>
+<script defer src="<?= $esc('/admin-assets/js/navigation-admin.js?v=wu3-dismissal') ?>"></script>
