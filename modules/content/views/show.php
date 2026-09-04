435ac049f0be9d9eaad73e5e9a0dd04d152f57b8
blackdjurix <blackdjurix@gmail.com>
2026-09-04 23:12:56 +0700

    fix(wu4): order general page content

--- modules/content/views/show.php
+++ modules/content/views/show.php
@@ -15,15 +15,15 @@ $featuredMedia = $context['featuredMedia'] ?? null;
         <?php endif; ?>
         <h1><?= htmlspecialchars((string) ($entry['title'] ?? ''), ENT_QUOTES, 'UTF-8') ?></h1>
 
-        <?php if (is_array($featuredMedia ?? null)): ?>
-            <img class="builtin-site-featured-image" src="<?= htmlspecialchars((string) $featuredMedia['url'], ENT_QUOTES, 'UTF-8') ?>"<?= !empty($featuredMedia['srcset']) ? ' srcset="' . htmlspecialchars((string) $featuredMedia['srcset'], ENT_QUOTES, 'UTF-8') . '" sizes="(max-width: 720px) 100vw, 720px"' : '' ?> width="<?= (int) ($featuredMedia['width'] ?? 0) ?>" height="<?= (int) ($featuredMedia['height'] ?? 0) ?>" alt="<?= htmlspecialchars((string) ($featuredMedia['alt'] ?? ''), ENT_QUOTES, 'UTF-8') ?>" loading="eager">
-        <?php endif; ?>
-
         <?php if (($entry['excerpt'] ?? null) !== null): ?>
             <p class="builtin-site-page__intro"><?= nl2br(htmlspecialchars((string) $entry['excerpt'], ENT_QUOTES, 'UTF-8')) ?></p>
         <?php endif; ?>
 
         <?php if (($entry['published_at'] ?? null) !== null): ?><p class="builtin-site-page__meta">Published <?= htmlspecialchars((string) $entry['published_at'], ENT_QUOTES, 'UTF-8') ?></p><?php endif; ?>
+
+        <?php if (is_array($featuredMedia ?? null)): ?>
+            <img class="builtin-site-featured-image" src="<?= htmlspecialchars((string) $featuredMedia['url'], ENT_QUOTES, 'UTF-8') ?>"<?= !empty($featuredMedia['srcset']) ? ' srcset="' . htmlspecialchars((string) $featuredMedia['srcset'], ENT_QUOTES, 'UTF-8') . '" sizes="(max-width: 720px) 100vw, 720px"' : '' ?> width="<?= (int) ($featuredMedia['width'] ?? 0) ?>" height="<?= (int) ($featuredMedia['height'] ?? 0) ?>" alt="<?= htmlspecialchars((string) ($featuredMedia['alt'] ?? ''), ENT_QUOTES, 'UTF-8') ?>" loading="eager">
+        <?php endif; ?>
 
         <div>
             <?= nl2br(htmlspecialchars((string) ($entry['body'] ?? ''), ENT_QUOTES, 'UTF-8')) ?>
